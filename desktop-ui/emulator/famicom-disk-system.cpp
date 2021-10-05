271da0fda2dc98a113b98d9f0fe6ce762166c5bc
Shideravan <jmrfmetal@gmail.com>
2021-10-04 22:23:45 -0300

    Update famicom-disk-system.cpp

--- desktop-ui/emulator/famicom-disk-system.cpp
+++ desktop-ui/emulator/famicom-disk-system.cpp
@@ -27,7 +27,7 @@ FamicomDiskSystem::FamicomDiskSystem() {
     device.digital("A",          virtualPorts[id].pad.b);
     device.digital("Select",     virtualPorts[id].pad.select);
     device.digital("Start",      virtualPorts[id].pad.start);
-    device.digital("Microphone", virtualPorts[id].pad.x);
+    device.digital("Microphone", virtualPorts[id].pad.r1);
     port.append(device); }
 
     ports.append(port);
